79d19ec98e4d0cc6f2db121733cfaedcf58bcb4c
JANACA <jaumenadaldev@gmail.com>
2024-11-04 02:05:06 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Backend en Nest
+
+1. `docker compose up -d`
+
+2. Copiar el `.env.template` y renombrarlo a `.env`
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './auth/auth.module';
+import { log } from 'console';
+
+@Module({
+  imports: [
+    
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot( process.env.MONGO_URI, { dbName: process.env.DB_NAME } ),
+
+    AuthModule,
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+
+}
